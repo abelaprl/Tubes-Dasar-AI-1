cc130ecaf407c2af20b1f8dc75db1cc02a05682c
khansadilla <117127484+khansadilla@users.noreply.github.com>
2024-11-11 00:23:15 +0700

    fixeddddd

---
+++ SimulatedAnnealing.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include "LocalSearch.cpp"
+
+using namespace std;
+
+double calculateTemperature(double Temperature, int time) {
+    return Temperature - 0.000000001*time; //Nilai temperature dikurang dengan 0.000000001*time agar turun secara perlahan
+}
+
+void simulatedAnnealing(CUBE &current) {
+    double threshold = 0.55;
+    int MAX_TIME = 100000;
+    int time = 0;
+    CUBE bestState = current;
+    int bestValue = findValue(current,0);
+
+    vector<double> probability_values;  
+    int stuck_count = 0;                
+    int unchanged_iterations = 0;     
+    const int stuck_threshold = 10; 
+
+    double temperature = 2;
+
+    while (time <= MAX_TIME) {
+        temperature=calculateTemperature(temperature,time);
+        if(temperature<=0) {cout << "Total stuck count (local optima): " << stuck_count << endl; return;}
+        CUBE neighbor = randomSuccessor(current);
+        int neighborValue = findValue(neighbor,0);
+
+        int deltaE = neighborValue - findValue(current,0);
+        double probability;
+        if(deltaE>0) probability=1;
+        else probability = exp(double(deltaE) / temperature); // e^(deltaE / T)
+        cout << "Iteration: " << time
+                << ", Temperature: " << temperature
+                << ", DeltaE: " << deltaE
+                << ", Probability (e^(DeltaE/T)): " << probability
+                << ", Threshold: " << threshold
+                << ", Value: " << current.value << endl;
+
+        if (deltaE > 0) {
+            current = neighbor;
+        } else {
+            if((probability > threshold && ((double)rand() / RAND_MAX) < probability)) current=neighbor;
+            stuck_count++;
+        }
+        time++;
+    }
+}
+
+int main() {
+    clock_t start, end;
+    start = clock();
+    srand(time(0));
+    CUBE current;
+    generateInitialState(&current);
+
+    cout << "Initial State:" << endl;
+    printState(current);
+    cout << "Initial Value: " << current.value << endl;
+
+     
+    
+    simulatedAnnealing(current);
+
+    cout << "Final State after Simulated Annealing:" << endl;
+    printState(current);
+    cout << "Final Value: " << findValue(current,0) << endl;
+     end = clock();
+ 
+    // Calculating total time taken by the program.
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cout << "Time taken by program is : " << fixed 
+         << time_taken << setprecision(5);
+    cout << " sec " << endl;
+    return 0;
+}
---
+++ genetic.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h> 
+#include "LocalSearch.cpp"
+using namespace std; 
+
+CUBE population[100];
+int POPULATION_SIZE;
+int ITERATION_TOTAL;
+
+void selection()
+{
+    int fitnessTotal=0;
+    double percentageFitnessLevel[POPULATION_SIZE];
+    for(int i=0; i<POPULATION_SIZE; i++)
+    {
+        fitnessTotal+=population[i].value;
+    }
+    for(int i=0; i<POPULATION_SIZE; i++)
+    {
+        percentageFitnessLevel[i]=double(population[i].value)/double(fitnessTotal);
+    }
+
+    double fitnessOrder[POPULATION_SIZE];
+    fitnessOrder[0] = 0;
+    for(int i=1; i<POPULATION_SIZE; i++)
+    {
+        fitnessOrder[i] = fitnessOrder[i-1] + double(population[i].value);
+    }
+    double maxNumber = fitnessOrder[POPULATION_SIZE-1];
+    fitnessOrder[POPULATION_SIZE-1] = 100;
+    
+    CUBE parent[POPULATION_SIZE];
+    for (int i=0; i<POPULATION_SIZE; i++) {
+        double randomNumber = (rand()%101);
+        for (int j=0; j<POPULATION_SIZE; j++) {
+            if ((fitnessOrder[j]) > randomNumber) parent[i] = population[j];
+        }
+    }    
+    for(int i=0; i<POPULATION_SIZE; i++)
+    {
+        population[i]=parent[i];
+    }
+}
+
+void swapPair(int x, int y, int num, int l, int m, int n)
+{
+    for(int i=2; i<=4; i++)
+    {
+        for(int j=1; j<=5; j++)
+        {
+            for(int k=1; k<=5; k++)
+            {
+                if(population[x].state[i][j][k]==num)
+                {
+                    num=population[y].state[i][j][k];
+                    i=2; j=1; k=1;
+                }      
+            }
+        }
+    }
+    for(int i=1; i<=5; i++)
+    {
+        if(i>=2 && i<=4) continue;
+        for(int j=1; j<=5; j++)
+        {
+            for(int k=1; k<=5; k++)
+            {
+                if(population[y].state[i][j][k]==num)
+                {
+                    swap(population[x].state[l][m][n],population[y].state[i][j][k]);
+                }
+            }
+        }
+    }
+}
+
+bool isDouble(int x, int num)
+{
+    for(int i=2; i<=4; i++)
+    {
+        for(int j=1; j<=5; j++)
+        {
+            for(int k=1; k<=5; k++)
+            {
+                if(population[x].state[i][j][k]==num) return true;        
+            }
+        }
+    }
+    return false;
+}
+void crossover(int x, int y)
+{
+    for(int i=2; i<=4; i++)
+    {
+        for(int j=1; j<=5; j++)
+        {
+            for(int k=1; k<=5; k++)
+            {
+                swap(population[x].state[i][j][k],population[y].state[i][j][k]);        
+            }
+        }
+    }
+    for(int i=1; i<=5; i++)
+    {
+        if(i>=2 && i<=4) continue;
+        for(int j=1; j<=5; j++)
+        {
+            for(int k=1; k<=5; k++)
+            {
+                if(isDouble(x,population[x].state[i][j][k]))
+                {
+                    swapPair(x,y,population[x].state[i][j][k],i,j,k);
+                }
+            }
+        }
+    }
+
+}
+
+int main() {
+    srand(time(0));
+    cin>>POPULATION_SIZE>>ITERATION_TOTAL;
+    bool found = false; 
+
+    // current generation 
+	int generation = 0; 
+    // generate population
+	for(int i=0; i<POPULATION_SIZE; i++) 
+	{ 
+        CUBE gnome;
+		generateInitialState(&gnome); 
+        population[i] = gnome;
+        printState(population[i]);
+	} 
+    
+	while (!found && generation<ITERATION_TOTAL)
+	{ 
+		// sort the population in increasing order of fitness score 
+		//sort(population,population+POPULATION_SIZE); 
+
+		// if the individual having 0 fitness score then we know that we have reached to the target 
+		// so break the loop 
+        for(int i=0; i<POPULATION_SIZE; i++)
+        {
+            if(population[POPULATION_SIZE-1].value == 109) 
+            { 
+                found = true; 
+                break; 
+            } 
+        }
+        selection();
+        for(int i=0; i<POPULATION_SIZE; i+=2)
+        {
+            crossover(i,i+1);
+        }
+        //mutation
+        for (int i=0; i<POPULATION_SIZE; i++) {
+            population[i]=randomSuccessor(population[i]);
+        }
+        generation++;
+    }
+    cout<<"FINAL STATE:"<<endl;
+    for(int i=0; i<POPULATION_SIZE; i++)
+    {
+        printState(population[i]);
+    }
+}
